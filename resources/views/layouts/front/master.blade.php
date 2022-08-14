52a53edf94bec3b1e5e37aebdd6109cb4aa54079
rasool_sh <rasool.sh344@gmail.com>
2022-08-14 20:09:17 +0430

    start project

---
+++ resources/views/layouts/front/master.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="fa" dir="rtl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>کریپتولوژی</title>
+    <link rel="shortcut icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.rtl.min.css" integrity="sha384-+4j30LffJ4tgIMrq9CwHvn0NjEvmuDCOfk6Rpg2xg7zgOxWWtLtozDEEVvBPgHqE" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="{{asset('css/custom.css')}}">
+    @yield('style')
+</head>
+
+<body>
+<div id="main">
+    @yield('content')
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous" defer></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+@yield('script')
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,109 @@
+@extends('layouts.front.master')
+@section('content')
+    @include('layouts.front.nav-sticky')
+    <section id="header">
+        <div class="container">
+
+            <nav class="navbar navbar-dark navbar-expand-md bg-transparent py-3">
+                <div class="container-fluid mx-3">
+                    <a class="navbar-brand" href="#">Cryptologi.ir</a>
+
+
+                    <div class="ms-auto">
+                        <a href="" class="btn btn-info text-light">ورود <i class="bi bi-box-arrow-in-right"></i></a>
+                        <a href="" class="btn btn-success">عضویت <i class="bi bi-person-plus"></i></a>
+                    </div>
+
+                    <button class="navbar-toggler ms-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#nav_offcanvas">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+
+            </nav>
+
+
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark px-5 py-2 rounded-4 d-md-flex d-none">
+                <div class="collapse navbar-collapse d-flex justify-content-between">
+
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a href="" class="nav-link">صفحه اصلی</a></li>
+                        <li class="nav-item"><a href="" class="nav-link">دوره های آموزشی</a></li>
+                        <li class="nav-item"><a href="" class="nav-link">مقالات</a></li>
+                        <li class="nav-item"><a href="" class="nav-link">چرا کریپتولوژی ؟</a></li>
+                        <li class="nav-item"><a href="" class="nav-link">تماس با ما</a></li>
+                    </ul>
+
+                    <div class="text-center">
+                        <a href="" class="nav-item"><i class="bi bi-instagram"></i></a>
+                        <a href="" class="nav-item"><i class="bi bi-telegram"></i></a>
+                        <a href="" class="nav-item"><i class="bi bi-telephone"></i></a>
+                    </div>
+
+
+                </div>
+            </nav>
+
+
+            <div class="text-center text-light mt-6">
+                <p class="mt-3" style="font-size: 26px;">هدف ما افزایش تخصص شماست</p>
+            </div>
+        </div>
+    </section>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+@endsection
+
+@section('script')
+    <script>
+        $(document).ready(function() {
+            $('#nav_sticky').hide();
+            $(window).scroll(function() {
+                if ($(document).scrollTop() > 320) {
+                    $('#nav_sticky').fadeIn();
+                } else {
+                    $('#nav_sticky').hide();
+                }
+            });
+        });
+    </script>
+@endsection
+
+
